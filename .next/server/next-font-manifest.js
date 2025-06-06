44c35788afd0fe69c0e18ccec9fac919c97b2a26
Anderson-Vasques <anderson.vasques.da.rosa@gmail.com>
2025-06-05 23:29:03 -0300

    Project setup

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/Anderson-Vasques/hangman-assessment/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HtyJyk2cPcCa7nOlLA/cIgW2R06tQ358VAMumukhv4g=\"\n}"
